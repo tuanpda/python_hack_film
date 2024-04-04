71efcaff4e9d6383b4740d667b665a4ec479634a
Your Name <you@example.com>
2024-04-04 20:48:38 +0700

    git python

--- keophim_zuiphim.py
+++ keophim_zuiphim.py
@@ -510,7 +510,7 @@ def job():
 scheduler = BlockingScheduler()
 
 # Lập lịch cho công việc chạy vào mỗi ngày vào 17:45
-scheduler.add_job(job, "cron", hour=17, minute=48)
+scheduler.add_job(job, "cron", hour=20, minute=50)
 
 # Lập lịch cho công việc chạy cứ mỗi 10 giờ kể từ 21:45 hàng ngày
 scheduler.add_job(job, "interval", hours=10)
